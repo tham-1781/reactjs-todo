47d1d1e67486fa6384640c23eccf3fe80c674219
Tham <thamdavies@gmail.com>
2019-03-11 00:00:47 +0700

    Finish edit tasks.

--- src/containers/task-form.js
+++ src/containers/task-form.js
@@ -1,81 +1,46 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from "react-redux";
-import { addTask } from "../actions/index";
+import { addTask, editTask } from "../actions/index";
 
-function mapDispatchToProps(dispatch) {
-  return {
-    addTask: task => dispatch(addTask(task))
-  };
+const TaskForm = ({ dispatch, task }) => {
+  const buttonClass = task ? 'editBtn' : 'addBtn';
+  let formClass = task ? 'add-task edit-mode' : 'add-task';
+  let input;
+
+  return (
+    <form className={formClass}
+      onSubmit={(event) => handleOnSubmit(event, dispatch, task, input)}>
+      <input placeholder="Enter title..."
+        ref={node => (input = node)} defaultValue={task ? task.title : ''} />
+      {renderActionButtons(buttonClass, task)}
+    </form>
+  );
 }
 
-class ConnectedTaskForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isEditing: this.props.isEditing || false
-    };
-  }
-
-  renderError = () => {
-    if (!this.state.error) return null;
-    return <div className="error">{this.state.error}</div>
-  }
-
-  render() {
-    const buttonClass = this.state.isEditing ? 'editBtn' : 'addBtn';
-    let formClass = this.state.isEditing ? 'add-task edit-mode' : 'add-task';
-
-    formClass = this.state.error ? formClass += ' has-error' : formClass;
-
+const renderActionButtons = (buttonClass, task) => {
+  if (!task)
+    return <button type="submit" className={buttonClass}>Add</button>;
+  else {
     return (
-      <form className={formClass}
-        onSubmit={this.state.isEditing ? this.handleEdit : this.addTask}>
-        {this.renderError()}
-        <input placeholder="Enter title..." ref="taskInput"
-          defaultValue={this.props.task ? this.props.task.title : ''} />
-        {this.renderActionButtons(buttonClass)}
-      </form>
+      <React.Fragment>
+        <button type="submit" className={buttonClass}>save</button>
+        <button type="button"
+          className={buttonClass}>cancel</button>
+      </React.Fragment>
     );
   }
-
-  renderActionButtons = (buttonClass) => {
-    if (!this.state.isEditing)
-      return <button type="submit" className={buttonClass}>Add</button>;
-    else {
-      return (
-        <React.Fragment>
-          <button type="submit" className={buttonClass}>save</button>
-          <button type="button" onClick={this.props.handleCancelClick}
-            className={buttonClass}>cancel</button>
-        </React.Fragment>
-      );
-    }
-  }
-
-  addTask = (event) => {
-    event.preventDefault();
-    const taskInput = this.refs.taskInput;
-    const validateTitle = this.validateTitle(taskInput.value);
-
-    if (validateTitle) {
-      this.setState({ error: validateTitle });
-      return;
-    }
-
-    this.setState({ error: null });
-    this.props.addTask(taskInput.value);
-    this.refs.taskInput.value = '';
-    this.setState({ task: { title: taskInput.value, completed: false } });
-  }
-
-  validateTitle = (title) => {
-    if (!title)
-      return 'Please enter task title!!!';
-    return null;
-  }
 }
 
-const TaskForm = connect(null, mapDispatchToProps)(ConnectedTaskForm);
+const handleOnSubmit = (event, dispatch, task, input) => {
+  event.preventDefault();
+  if (task) {
+    dispatch(editTask(task.id, input.value));
+  }
+  else {
+    dispatch(addTask(input.value));
+  }
 
-export default TaskForm;
+  input.value = '';
+}
+
+export default connect()(TaskForm);
--- src/containers/visible-todo-list.js
+++ src/containers/visible-todo-list.js
@@ -1,18 +1,18 @@
 import { connect } from 'react-redux';
-import { toggleTask, deleteTask } from '../actions/index';
+import { toggleTask, deleteTask, editClick } from '../actions/index';
 import TodoList from '../components/todo-list';
 import { VisibilityFilters } from '../actions';
 
-const getVisibleTodos = (tasks, filter) => {
+const getVisibleTodos = (state, filter) => { 
   switch (filter) {
     case VisibilityFilters.SHOW_ALL:
-      return tasks;
+      return state;
 
     case VisibilityFilters.SHOW_COMPLETED:
-      return tasks.filter(t => t.completed);
+      return state.filter(t => t.completed);
 
     case VisibilityFilters.SHOW_ACTIVE:
-      return tasks.filter(t => !t.completed);
+      return state.filter(t => !t.completed);
 
     default:
       throw new Error('Unknown filter: ' + filter);
@@ -20,11 +20,14 @@ const getVisibleTodos = (tasks, filter) => {
 };
 
 
-const mapStateToProps = state => ({ tasks: getVisibleTodos(state.tasks, state.visibilityFilter) });
+const mapStateToProps = state => (
+  { tasks: getVisibleTodos(state.tasks, state.visibilityFilter) }
+  );
 
 const mapDispatchToProps = dispatch => ({
   toggleTask: id => dispatch(toggleTask(id)),
-  deleteTask: id => dispatch(deleteTask(id))
+  deleteTask: id => dispatch(deleteTask(id)),
+  editClick: id => dispatch(editClick(id))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
